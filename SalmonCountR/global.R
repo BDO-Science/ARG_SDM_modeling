71b5805c7a23e6e2ac0dea3b7fc1a881b39bf5cf
Mahardja <bmahardja@usbr.gov>
2025-09-25 11:51:31 -0700

    Removed hatchery objective and put placeholder values for steelhead and hydropower

--- SalmonCountR/global.R
+++ SalmonCountR/global.R
@@ -19,7 +19,7 @@ sim_years          <- readRDS(here("SalmonCountR", "app_data", "sim_years.rds"))
 spawn_dates_by_alt <- readRDS(here("SalmonCountR", "app_data","spawn_dates_by_alt.rds"))
 instream <- readRDS(here("SalmonCountR", "app_data","american_river_instream.rds"))
 # read csv data for non-salmon objectives
-otherobjectives <- read_csv(here::here("SalmonCountR", "app_data","nonsalmon_objectives.csv"))
+#otherobjectives <- read_csv(here::here("SalmonCountR", "app_data","nonsalmon_objectives.csv"))
 
 real_years <- 2011:2024 
 n_sim      <- 114
